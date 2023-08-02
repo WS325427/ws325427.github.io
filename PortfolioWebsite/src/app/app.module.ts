165689da0d0981c9850dc364171b3df7cfb860d7
Joshua Dalangin <joshua.dalangin@burohappold.com>
2023-08-02 23:09:31 +0100

    PortfolioWebsite Initialising

---
+++ PortfolioWebsite/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomePageComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
